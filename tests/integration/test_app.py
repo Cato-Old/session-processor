ad7b45f90c3d9b3c4edc85fb49e4b408fe1add46
Wojciech Oczkowski <wojciech.oczkowski@gmail.com>
2021-06-25 22:48:06 +0200

    Integrate stub application object with main entrypoint

--- tests/integration/test_app.py
+++ tests/integration/test_app.py
@@ -1,3 +1,4 @@
+import os
 from os.path import dirname
 from os.path import join
 from subprocess import run
@@ -7,10 +8,20 @@ from pytest import fixture
 
 class TestApp:
     @fixture
-    def path(self) -> str:
-        return join(dirname(__file__), '..', '..', 'src', 'app')
+    def project_path(self) -> str:
+        return join(dirname(__file__), '..', '..')
 
-    def test_is_app_running(self, path: str) -> None:
+    @fixture
+    def path(self, project_path: str) -> str:
+        return join(project_path, 'src', 'main.py')
+
+    @fixture
+    def env(self, project_path: str) -> dict:
+        env = os.environ
+        env.update({'PYTHONPATH': project_path})
+        return env
+
+    def test_is_app_running(self, path: str, env: dict) -> None:
         arguments = ['python', path]
-        result = run(arguments)
-        assert result.returncode == 0
+        result = run(arguments, env=env)
+        assert result.returncode == 1
