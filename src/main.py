900014a5b5c5793935fab45b125ecb1d0644f8cd
Wojciech Oczkowski <wojciech.oczkowski@gmail.com>
2021-06-26 07:18:47 +0200

    Inject session processor view to application object

--- src/main.py
+++ src/main.py
@@ -1,8 +1,14 @@
 from src.app import Application
+from src.view import SessionProcessorView
+
+
+def build_application() -> Application:
+    view = SessionProcessorView()
+    return Application(view=view)
 
 
 def main() -> None:
-    app = Application()
+    app = build_application()
     app.run()
 
 
--- tests/unit/test_app.py
+++ tests/unit/test_app.py
@@ -1,13 +1,19 @@
+from mockito import mock
 from pytest import fixture
 from pytest import raises
 
 from src.app import Application
+from src.view import SessionProcessorView
 
 
 class TestApplication:
     @fixture
-    def app(self) -> Application:
-        return Application()
+    def view(self) -> SessionProcessorView:
+        return mock()
+
+    @fixture
+    def app(self, view: SessionProcessorView) -> Application:
+        return Application(view=view)
 
     def test_raises_on_run(self, app: Application) -> None:
         with raises(NotImplementedError):
