7de711ce5dc94af93f870afdfb86920a4d3c93ab
Wojciech Oczkowski <wojciech.oczkowski@gmail.com>
2021-06-26 14:11:08 +0200

    Integrate loader with view

--- src/view.py
+++ src/view.py
@@ -3,7 +3,8 @@ from src.loader import Loader
 
 class SessionProcessorView:
     def __init__(self, loader: Loader) -> None:
-        pass
+        self._loader = loader
 
     def process(self, path: str) -> None:
+        self._loader.load(path)
         raise NotImplementedError
--- tests/unit/test_view.py
+++ tests/unit/test_view.py
@@ -1,10 +1,11 @@
 from factory.fuzzy import FuzzyText
-from mockito import mock
+from mockito import mock, when, verify
 from pytest import fixture
 from pytest import raises
 
 from src.loader import Loader
 from src.view import SessionProcessorView
+
 from tests.unit.test_loader import StatementFactory
 
 
@@ -14,15 +15,19 @@ class TestSessionProcessorView:
         return FuzzyText().fuzz()
 
     @fixture
-    def loader(self) -> Loader:
-        return mock()
+    def loader(self, path: str) -> Loader:
+        statements = StatementFactory.build_batch(10)
+        loader = mock()
+        when(loader).load(path).thenReturn(s for s in statements)
+        return loader
 
     @fixture
     def view(self, loader: Loader) -> SessionProcessorView:
         return SessionProcessorView(loader=loader)
 
     def test_raises_on_process(
-            self, view: SessionProcessorView, path: str,
+            self, view: SessionProcessorView, path: str, loader: Loader,
     ) -> None:
         with raises(NotImplementedError):
             view.process(path)
+        verify(loader).load(...)
