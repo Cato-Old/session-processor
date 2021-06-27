0740c724c350be82b234308a83f545dd8d595994
Wojciech Oczkowski <wojciech.oczkowski@gmail.com>
2021-06-27 07:42:47 +0200

    Implement sort method

--- src/sorter.py
+++ src/sorter.py
@@ -6,5 +6,9 @@ from src.domain import Statement
 
 class StatementSorter:
     @staticmethod
-    def sort(grouped_statement: Dict[int, List[Statement]]) -> None:
-        raise NotImplementedError
+    def sort(
+            grouped_statement: Dict[int, List[Statement]],
+    ) -> Dict[int, List[Statement]]:
+        for key in grouped_statement:
+            grouped_statement[key].sort(key=lambda s: s.start_time)
+        return grouped_statement
--- tests/unit/test_sorter.py
+++ tests/unit/test_sorter.py
@@ -2,7 +2,6 @@ from typing import Dict, List
 
 from factory.fuzzy import FuzzyInteger
 from pytest import fixture
-from pytest import raises
 
 from src.domain import Statement
 from src.sorter import StatementSorter
@@ -22,10 +21,19 @@ class TestStatementSorter:
             for no in home_numbers
         }
 
+    @fixture
+    def expected(
+            self, grouped_statements: Dict[int, List[Statement]],
+    ) -> Dict[int, List[Statement]]:
+        for key in grouped_statements:
+            grouped_statements[key].sort(key=lambda s: s.start_time)
+        return grouped_statements
+
     def test_raise_on_sort_method(
             self,
             sorter: StatementSorter,
             grouped_statements: Dict[int, List[Statement]],
+            expected: Dict[int, List[Statement]],
     ) -> None:
-        with raises(NotImplementedError):
-            sorter.sort(grouped_statements)
+        actual = sorter.sort(grouped_statements)
+        assert actual == expected
