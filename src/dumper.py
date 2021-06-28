d776103b0c7ed26b621c489017f032adaf8bb2b2
Wojciech Oczkowski <wojciech.oczkowski@gmail.com>
2021-06-28 14:26:41 +0200

    Implement dump method

--- src/dumper.py
+++ src/dumper.py
@@ -1,15 +1,37 @@
 from abc import ABC
 from abc import abstractmethod
+from csv import DictWriter
 
 from src.domain import SessionGenerator
 
 
 class Dumper(ABC):
     @abstractmethod
-    def dump(self, sessions: SessionGenerator) -> None:
+    def dump(self, sessions: SessionGenerator, path: str) -> None:
         raise NotImplementedError
 
 
 class PSVDumper(Dumper):
-    def dump(self, sessions: SessionGenerator) -> None:
-        raise NotImplementedError
+    def dump(self, sessions: SessionGenerator, path: str) -> None:
+        fieldnames = [
+            'HomeNo',
+            'Channel',
+            'Starttime',
+            'Activity',
+            'Endtime',
+            'Duration',
+        ]
+        rowdicts = [
+            {
+                'HomeNo': session.home_no,
+                'Channel': session.channel,
+                'Starttime': session.start_time.strftime('%Y%m%d%H%M%S'),
+                'Activity': session.activity,
+                'Endtime': session.end_time.strftime('%Y%m%d%H%M%S'),
+                'Duration': session.duration.total_seconds(),
+            } for session in sessions
+        ]
+        with open(path, mode='w', newline='') as f:
+            writer = DictWriter(f, fieldnames, delimiter='|')
+            writer.writeheader()
+            writer.writerows(rowdicts)
--- tests/unit/test_dumper.py
+++ tests/unit/test_dumper.py
@@ -1,7 +1,12 @@
-from pytest import fixture
-from pytest import raises
+import filecmp
+import os
+from csv import DictWriter
+from tempfile import NamedTemporaryFile
+from typing import List
 
-from src.domain import SessionGenerator
+from pytest import fixture
+
+from src.domain import SessionGenerator, Session
 from src.dumper import PSVDumper
 
 from tests.unit.controller.test_creator import SessionFactory
@@ -9,16 +14,57 @@ from tests.unit.controller.test_creator import SessionFactory
 
 class TestPSVDumper:
     @fixture
-    def sessions(self) -> SessionGenerator:
-        return (s for s in SessionFactory.build_batch(10))
+    def session_values(self) -> List[Session]:
+        return SessionFactory.build_batch(10)
+
+    @fixture
+    def sessions(self, session_values) -> SessionGenerator:
+        return (s for s in session_values)
 
     @fixture
     def dumper(self) -> PSVDumper:
         return PSVDumper()
 
-    def test_raises_on_dump(
-            self, dumper: PSVDumper, sessions: SessionGenerator,
-    ) -> None:
-        with raises(NotImplementedError):
-            dumper.dump(sessions)
+    @fixture
+    def expected(self, session_values: List[Session]) -> str:
+        fieldnames = [
+            'HomeNo',
+            'Channel',
+            'Starttime',
+            'Activity',
+            'Endtime',
+            'Duration',
+        ]
+        rowdicts = [
+            {
+                'HomeNo': session.home_no,
+                'Channel': session.channel,
+                'Starttime': session.start_time.strftime('%Y%m%d%H%M%S'),
+                'Activity': session.activity,
+                'Endtime': session.end_time.strftime('%Y%m%d%H%M%S'),
+                'Duration': session.duration.total_seconds(),
+            } for session in session_values
+        ]
+        with NamedTemporaryFile(mode='w', newline='', delete=False) as ntf:
+            writer = DictWriter(ntf, fieldnames, delimiter='|')
+            writer.writeheader()
+            writer.writerows(rowdicts)
+        yield ntf.name
+        os.remove(ntf.name)
 
+    @fixture
+    def path(self) -> str:
+        dirname = os.path.dirname(__file__)
+        path = os.path.join(dirname, 'output.psv')
+        yield path
+        os.remove(path)
+
+    def test_dumps_sessions_to_psv_file(
+            self,
+            dumper: PSVDumper,
+            sessions: SessionGenerator,
+            expected: NamedTemporaryFile,
+            path: str,
+    ) -> None:
+        dumper.dump(sessions, path)
+        assert filecmp.cmp(expected, path)
